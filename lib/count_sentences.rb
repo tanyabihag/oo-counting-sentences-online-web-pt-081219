25d6b341080eb31d25fb811e771d90226529070b
Tanya Bihag <tbihag@gmail.com>
2019-09-08 01:26:24 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,11 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+=======
+    new_array = self.split(/[.?!]/).reject
+>>>>>>> 88425822547da0405326b659145d74f60dac0261
     return new_array.length
   end
 end
